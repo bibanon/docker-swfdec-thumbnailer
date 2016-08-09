3d9b3050f5036ca7a8d2fce8274bf82b630adaf6
Antonizoon <antonizoon@bibanon.org>
2016-08-09 11:43:52 -0700

    added mass swf processor in run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+SWFDIR=`pwd`/swf
+
 # run the swfdec-thumbnailer on a single .swf file in a folder
-docker run -v `pwd`/swf:/swf swfdec-gnome swfdec-thumbnailer 10seconds.swf 10seconds.swf.jpg
+#docker run -v $SWFDIR:/swf swfdec-thumbnailer swfdec-thumbnailer 10seconds.swf 10seconds.swf.jpg
 
 # run the swfdec-thumbnailer on all .swf files in a folder
+docker run -v $SWFDIR:/swf swfdec-thumbnailer find . -maxdepth 1 -type f -exec swfdec-thumbnailer {} {}.jpg \;
+
+# there might be errors, but ignore them for the most part: especially audio decoding errors, because who cares, we just make thumbnails.
